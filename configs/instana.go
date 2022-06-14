3213bc05ddc120925dc15408a5e2bec0aa170cf4
Cayke Xavier <cayke.xavier@dafiti.com.br>
2022-06-14 17:29:22 -0300

    feat: Ajustes novo padrao de Log e pipeline

--- configs/instana.go
+++ configs/instana.go
@@ -3,15 +3,21 @@ package configs
 import (
 	"os"
 
+	"github.com/gin-gonic/gin"
 	instana "github.com/instana/go-sensor"
+	"github.com/instana/go-sensor/instrumentation/instagin"
 	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
 )
 
-func init() {
+func initAPM(router *gin.Engine) {
 	if os.Getenv("ENVIRONMENT") != "development" {
 		tracer := instana.NewTracerWithOptions(instana.DefaultOptions())
+		sensor := instana.NewSensorWithTracer(tracer)
+
 		opentracing.InitGlobalTracer(tracer)
 		instana.SetLogger(logrus.StandardLogger())
+
+		instagin.AddMiddleware(sensor, router)
 	}
 }
