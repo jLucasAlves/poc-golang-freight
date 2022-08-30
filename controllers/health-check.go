20cad2ed32983d94534e68131cd41820580527e3
Gritzko D. Kleiner <ext.gritzko.kleiner@dafiti.com.br>
2022-08-30 08:36:50 -0300

    [ci skip] update template
    - include the new lightweight health-checker
    - update dependencies
    - update orb to 1.0.4

--- controllers/health-check.go
+++ controllers/health-check.go
@@ -3,30 +3,32 @@ package controllers
 import (
 	"io/ioutil"
 
+	"github.com/dafiti-group/${{values.component_id}}/pkg/github"
 	"github.com/gin-gonic/gin"
-	"github.com/gritzkoo/golang-health-checker/pkg/healthcheck"
+	"github.com/gritzkoo/golang-health-checker-lw/pkg/healthchecker"
 	"github.com/sirupsen/logrus"
 )
 
+var version, _ = ioutil.ReadFile("rev.txt")
+var checker = healthchecker.New(healthchecker.Config{
+	Name:    "${{values.component_id}}",
+	Version: string(version),
+	Integrations: []healthchecker.Check{
+		{
+			Name:   "GitHub Api Integration",
+			Handle: github.Status, // you should write your own tests to pass here!
+		},
+	},
+})
+
 // HealthCheckLiveness show a simple check
 func HealthCheckLiveness(c *gin.Context) {
-	c.JSON(200, healthcheck.HealthCheckerSimple())
+	c.JSON(200, checker.Liveness())
 }
 
 // HealthCheckReadiness return a detailed status of all integrations in the list
 func HealthCheckReadiness(c *gin.Context) {
-	version, _ := ioutil.ReadFile("rev.txt")
-	result := healthcheck.HealthCheckerDetailed(healthcheck.ApplicationConfig{
-		Name:    "${{values.component_id}}",
-		Version: string(version),
-		Integrations: []healthcheck.IntegrationConfig{
-			{
-				Type: healthcheck.Web,
-				Name: "Just a simple test",
-				Host: "https://github.com/status",
-			},
-		},
-	})
+	result := checker.Readiness()
 	if !result.Status {
 		// the line below will print the error on logs, so you need to
 		// configure grafana or instana alarms with this context
